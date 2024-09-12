36d1f556ece5bd6530d8d8f4c8f55808948b2890
Mostafahamady <Mostafa.hamady@gmail.com>
2024-09-12 09:11:08 +0300

    Adding new Service

--- GItSample/Controllers/HomeController.cs
+++ GItSample/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace GItSample.Controllers
         {
             return View();
         }
+        public IActionResult Display()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
